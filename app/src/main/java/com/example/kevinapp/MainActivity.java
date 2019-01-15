145f364b2ac1daaf274e391cef2c08279c9ae73a
Thien Truong <tcthien@gmail.com>
2019-01-15 14:24:10 -0500

    Add vibration and minor cosmetics

--- app/src/main/java/com/example/kevinapp/MainActivity.java
+++ app/src/main/java/com/example/kevinapp/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.kevinapp;
 
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    float origPosY = 0;
 
     ObjectAnimator objAnim;
     private void pulseAnimation(){
@@ -24,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         objAnim.start();
     };
 
+    ValueAnimator colorAnim;
+    private void startColorAnimation(View v){
+        colorAnim = ObjectAnimator.ofInt(v, "backgroundColor", v.getSolidColor(), 0xffff0000);
+        colorAnim.setDuration(50);
+        colorAnim.setEvaluator(new ArgbEvaluator());
+        colorAnim.setRepeatCount(ObjectAnimator.RESTART);
+        colorAnim.setRepeatMode(ObjectAnimator.REVERSE);
+        colorAnim.start();
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,27 +46,15 @@ public class MainActivity extends AppCompatActivity {
         final Button buttonClose = (Button) (findViewById(R.id.buttonClose));
 
         final TextView textViewQuestion = (TextView) findViewById(R.id.textViewQuestion);
-        final TextView textViewThank = (TextView) findViewById(R.id.textViewThank);
-
-
-//        origPosY = buttonNo.getY();
-//        System.out.println(buttonYes.getScaleX());
-//        System.out.println(buttonYes.getScaleY());
-
-        System.out.println(origPosY);
-
 
         this.pulseAnimation();
-
 
         buttonYes.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 objAnim.cancel();
-                findViewById(R.id.Screen).setBackgroundColor(Color.GREEN);
-//                buttonYes.setScaleX(1);
-//                buttonYes.setScaleY(1);
-                textViewQuestion.setText(textViewQuestion.getText()+ "\nYou did say Yes");
+                findViewById(R.id.constrainLayout).setBackgroundColor(Color.GREEN);
+                textViewQuestion.setText(textViewQuestion.getText()+ "\nYou did say YES");
                 findViewById(R.id.textViewThank).setVisibility(View.VISIBLE);
                 buttonYes.setVisibility(View.INVISIBLE);
                 buttonNo.setVisibility(View.INVISIBLE);
@@ -66,18 +66,13 @@ public class MainActivity extends AppCompatActivity {
         buttonNo.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                findViewById(R.id.textViewThank).setVisibility(View.INVISIBLE);
                 if (!objAnim.isRunning())
                     objAnim.start();
-//                float bottomY = findViewById(R.id.Screen).getHeight()-buttonNo.getHeight();
-//                float currentY = buttonNo.getY() + buttonNo.getHeight();
-//                System.out.println("Y=" + currentY + findViewById(R.id.Screen).getY());
-                findViewById(R.id.Screen).setBackgroundColor(Color.RED);
-
-//                if (currentY < bottomY)
-//                    buttonNo.setY(currentY);
-//                else
-//                    buttonNo.setY(origPosY);
+                startColorAnimation(findViewById(R.id.constrainLayout));
+                Vibrator vi = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Each element then alternates between vibrate, sleep, vibrate, sleep...
+                long[] pattern = {0, 100};
+                vi.vibrate(pattern, -1);
             }
         });
 
